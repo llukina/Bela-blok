b340645f5ef39ce0f52569f318dbbdcc1172ccf9
llukina <luka.lukina@gmail.com>
2022-03-06 17:47:37 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LinkWrapperComponent } from './components/link-wrapper/link-wrapper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LinkWrapperComponent
   ],
   imports: [
     BrowserModule,
